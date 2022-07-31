97fe6aed67e3f9c341cff2a7d4df9867f73d39d9
Danya <kloveazaks@gmail.com>
2022-07-31 12:05:56 +0300

    add authserver

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require("express");
 const app = express();
-
 const jwt = require('jsonwebtoken');
 
 app.use(express.json());
@@ -21,7 +20,7 @@ const posts = [
 ];
 
 app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts);
+  res.json(posts.filter(post => post.username === req.user.name));
 });
 
 app.post('/login', (req, res) => {
@@ -41,7 +40,9 @@ function authenticateToken(req, res, nex) {
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(403); 
+    req.user = user;
+    next();
   });
 };
 
